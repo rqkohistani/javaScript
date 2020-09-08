37ac882afb7be0e236e2a39e064e1f5a177624f9
Rashed Qazizada <rashidqk@gmail.com>
2020-09-08 13:48:39 +0200

    Data types primitive&reference

--- app.js
+++ app.js
@@ -1,41 +1,86 @@
-// var, let , const
 
-var name = 'John Doe';
-console.log(name);
-name = 'Steve Smith';
-console.log(name);
-// initialize var
-var greeting;
-console.log(greeting);
-greeting = 'Hello';
-console.log(greeting);
-// LET
-let secondName;
-secondName = 'Rashed';
-console.log(secondName);
 
-// CONST
-const firstName = 'Adam';
-console.log(firstName);
-// Have to asign a value.
-// const greet;
 
-const person = {
-  name: 'D-building',
-  age: 32,
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+Primitive data types: Stored directly in the location the variable access. Stored on the stack
+String, Number, Boolean, Null, Undefined, Symbols(ES6)
+
+Reference Data Types: Accessed by reference.
+Object that are stored on the heap
+A pointer to a location in memory.
+Arrays, object leterals, Functions, Dates....
+*/
+// Primitive
+// String
+const name = 'Rashed';
+// numbers
+const age = 32;
+console.log(typeof name);
+console.log(typeof age);
+// boolean
+const hasKids = true;
+console.log(typeof hasKids);
+//Null
+const car = null;
+console.log(typeof car);
+
+// undefined
+let test;
+console.log(typeof test);
+// symbol
+const sys = Symbol();
+console.log(typeof sys);
+
+// Reference Type
+
+// Array
+const hobbies = ['movies', 'musice'];
+console.log(typeof hobbies);
+
+// object literal
+const address = {
+  city: 'Växjö',
+  state: 'kronoberg'
 }
-console.log(person);
-person.name = 'Rashed',
-  person.age = 30,
-  console.log(person);
-// you can see that we could change the name 
-// it is the object name person that cannot be
-//reassigned . not the contents inside.
-
-const numbers = [1, 2, 3, 4, 5];
-console.log(numbers);
-numbers.push(6);
-// we can add elements inside however we cannot
-numbers = [7, 8, 9];
-
-
+console.log(typeof address);
+const today = new Date();
+console.log(today);
+console.log(typeof today);
