f99bcea910357594ed9b83ad4ea9ec5a4db25511
Rashed Qazizada <rashidqk@gmail.com>
2020-09-08 16:43:01 +0200

    Template literal

--- app.js
+++ app.js
@@ -1,58 +1,36 @@
-let val;
-
-// Number to string
-val = 5;
-// output
-console.log(val);
-console.log(typeof val);
-// val.length works if the type is string
-console.log(val.length);
-// Number to String
-val = String(55);
-val = String(4 + 4);
-// bool to string
-val = String(true);
-
-// date to string
-val = String(new Date);
-// array to string
-val = String([1, 2, 3, 4, 5]);
-
+const name = 'Rashed';
+const age = 32;
+const job = 'Student';
+const uni = 'linnaeus';
+let html;
+// without templete string (ES5)
+html = '<ul><li>Name: ' + name + '</li><li>Age: ' + age + '</li><li>job: ' + job + '</li><li>University: ' + uni + '</li></ul>';
 
 // OR
-val = (5).toString();
-val = (true).toString(),
-  // output
-  console.log(val);
-console.log(typeof val);
-// val.length works if the type is string
-console.log(val.length);
+html = '<ul>' +
+  '<li>Name: ' + name + ' </li>' +
+  '<li>Age: ' + age + ' </li>' +
+  '<li>Job: ' + job + ' </li>' +
+  '<li>University: ' + uni + ' </li>';
+'</ul>'
 
-// String to number
-let num = '5';
-console.log(num);
-// console.log(num.toFixed(2));
-console.log(typeof num);
-console.log(num.length);
-console.log('------------------')
-num = Number('5');
-console.log(num);
-num = Number(true);
-num = Number(false);
-num = Number(null);
-num = Number('Hello');
-num = Number([1, 2, 3, 4]);
-console.log(typeof num);
-console.log(num);
-console.log(num.length);
-console.log(num.toFixed(3));
-// NaN: Not a number
-console.log('------------------')
-num = parseInt('100.30');
-num = parseFloat('100.30');
-console.log(typeof num);
-console.log(num);
-console.log(num.toFixed(3));
+function hello() {
+  return 'Hello';
+}
+// with template strings ES6
+// html=``
+html = `
+  <ul>
+  <li>Name: ${name}</li>
+  <li>Age: ${age}</li>
+  <li>job: ${job}</li>
+  <li>University: ${uni}</li>
+  <li>${4 + 4}</li>
+  <li>${hello()}</li>
+  <li>${age > 31 ? 'Over 30' : 'Under 30'}</li>
+  </ul> 
+  `;
 
 
 
+document.body.innerHTML = html;
