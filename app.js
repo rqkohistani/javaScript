b151fc46e80d46282c660f44016c767ad3988870
Rashed Qazizada <rashidqk@gmail.com>
2020-09-08 11:08:02 +0200

    Basic JavaScript console.log

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-alert('Hello world! ')
+// alert('Hello world! ');
+// Log to console
+console.log("hello world");
+console.log('hello world');
+console.log(123);
+console.log(true);
+console.log(false);
+var greeting = 'Hello';
+console.log(greeting);
+console.log([1, 2, 3, 4]);
+// object
+console.log({ a: 1, b: 2 });
+// table
+console.table({ a: 1, b: 2 });
+console.error('This is an error message test');
+console.clear();
+console.warn('This is a warning test message');
+// to find out Time of exectuion of a method.
+console.time('Test');
+console.log('hello world');
+console.log('hello world');
+console.log('hello world');
+console.log('hello world');
+console.log('hello world');
+// Should the time execution 
+console.timeEnd('Test');
