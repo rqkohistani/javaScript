4c9cdc78ce6fdc07ed1e5efa7c20dd204a0e81bd
Rashed Qazizada <rashidqk@gmail.com>
2020-09-09 08:24:39 +0200

    Switch

--- app.js
+++ app.js
@@ -1,52 +1,17 @@
-// if-statements
-
-
-let id = 100;
-// EQAUL TO
-if (id == 100) {
-  console.log('CORRECT');
-} else {
-  console.error('INCORRECT');
-}
-
-// NOT EQUAL TO
-if (id != 101) {
-  console.log('CORRECT');
-} else {
-  console.log('Incorrect');
+let day;
+switch (new Date().getDay()) {
+  case 0:
+    day = 'Sunday';
+    break;
+  case 1:
+    day = 'Monday';
+    break;
+  case 3:
+    day = 'Tuesday';
+    break;
 
 }
-// EQAUL TO VALUE AND TYPE
-// mostly used === 
-id = '100';
-if (id === 100) {
-  console.log('CORRECT Type and value');
-  console.log(typeof id);
-} else {
-  console.log('INCORRECT ');
-  console.log(typeof id + ' type, it should be a number');
-}
-
-// // const testId = 1100;
-// if (testId) {
-//   console.log(`The ID is ${testId}`);
-// }
-// else {
-//   console.log('NO ID');
-// }
-//TO TEST IF SOMETHING IS UNDEFINED OR IF IT IS NOT THERE AT ALL THEN YOU SHOULD USE THIS.
-// TEST IF UNDEFINED
-if (typeof noVariableDefined !== 'undefined') {
-  console.log(`The ID is ${noVariableDefined}`);
-} else {
-  console.log('NO ID FOUND');
-}
-const name = 'Rashed';
-const age = 32;
-if (age <= 28 && age >= 60) {
-  console.log(`${name} is under age`);
-} else {
-  console.log(`${name} is valid age`);
-}
-// OR || && works a like java
-console.log(name === 'Rashed' ? 'CORRECT' : 'INCORRECT');
+// let val = new Date().getDay();
+// console.log(val)
+console.log(new Date().getDay());
+console.log(`Today is ${day}`);
