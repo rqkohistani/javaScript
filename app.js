456812f05dcbd0dc99eb2ffb9a4de8eb1648fe6d
Rashed Qazizada <rashidqk@gmail.com>
2020-09-09 08:47:09 +0200

    Function declarations

--- app.js
+++ app.js
@@ -1,17 +1,34 @@
-let day;
-switch (new Date().getDay()) {
-  case 0:
-    day = 'Sunday';
-    break;
-  case 1:
-    day = 'Monday';
-    break;
-  case 3:
-    day = 'Tuesday';
-    break;
+// FUNCTION DECLARAIONS
 
+function greet(firstName = "defualt", lastName = 'Default') {
+  return 'Hello ' + firstName + ' ' + lastName;
 }
-// let val = new Date().getDay();
-// console.log(val)
-console.log(new Date().getDay());
-console.log(`Today is ${day}`);
+
+console.log(greet());
+console.log(greet('Rashed', 'Qazizada'));
+// overwritting the default values
+
+// FUNCTION EXPRESSIONS
+// default value
+// const square = function (x = 3) {
+const square = function (x) {
+  return x * x;
+};
+console.log(square(2));
+
+// PROPERTIES METHOD
+const todo = {
+  add: function () {
+    console.log('Add todo...');
+  },
+  edit: function (id) {
+    console.log(`Edit todo ${id}`);
+  }
+}
+// can be added an object here too
+todo.delete = function () {
+  console.log(`Delete todo...`);
+}
+todo.add();
+todo.edit(22);
+todo.delete();
